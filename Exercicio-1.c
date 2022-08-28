571107416da396deb2e03274535e011f0859a5c4
Richard do Carmo Lopes <32149536+RichardCLopes@users.noreply.github.com>
2022-08-28 17:56:37 -0300

    Criacao do exercicio 1

---
+++ Exercicio-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void troca(int *a, int *b)
+{
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+} /* Fim de troca */
+
+int main (void)
+ {
+    int x, y;
+    printf ("Number 1:");
+    scanf ("%d", &x);
+    printf ("Number 2:");
+    scanf ("%d", &y);
+    troca (&x, &y);
+    printf ("\nTroca realizada:");
+    printf ("\nNumber 1: %d", x);
+    printf ("\nNumber 2: %d", y);
+    return 0;
+}
